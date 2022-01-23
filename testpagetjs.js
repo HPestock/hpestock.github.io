8a8c3276a4a59d7d6285f7db0764a7ab60e2c14c
HPestock <pestock22@gmail.com>
2022-01-22 20:00:30 -0600

    Update testpagetjs.js

--- testpagetjs.js
+++ testpagetjs.js
@@ -1,3 +1,15 @@
-console.log("test1");
-console.log("test2");
-alert("finished.");
+_X_GPROGAUTH = "testpagetjs";
+var n = _X_APIGETV("n");
+if(n===null){
+  n=0;
+  _X_APISTOREV("n",0);
+}else{
+  n++;
+  _X_APISTOREV("n",n);
+}
+_X_GOLDTEXT+=n+"\n";
+_X_GTEXTBUFFER = _X_GOLDTEXT + _X_GCURPREF + _X_GINPTB + _X_GCURTEXT;
+_X_APIRENDERTEXT();
+if(n>59){
+  _X_GPROGAUTH = "_X_MAIN";
+}
